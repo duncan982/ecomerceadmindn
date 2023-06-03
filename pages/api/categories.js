17c151f3a43b5f9cf87e298e4a8575dba515c8a8
duncan982 <ndegwaduncan@icloud.com>
2023-06-03 11:46:02 +0200

    updated categories to include to associate child and parent categories

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -4,10 +4,16 @@ import { Category } from "@/models/Category";
 export default async function handle(req, res) {
   const { method } = req;
   await mongooseConnect();
+  if (method === "GET") {
+    // console.log("req.body:", req.body);
+    res.json(await Category.find());
+  }
   if (method === "POST") {
-    const { name } = req.body;
-    console.log("req.body:", req.body);
-    const categoryDoc = await Category.create({ name });
+    const { name, parentCategory } = req.body;
+    // const { name } = req.body;
+    // console.log("req.body:", req.body);
+    // const categoryDoc = await Category.create({ name });
+    const categoryDoc = await Category.create({ name, parent: parentCategory });
     categoryDoc.save();
     res.json(categoryDoc);
   }
--- pages/categories.js
+++ pages/categories.js
@@ -1,14 +1,24 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Categories() {
   const [name, setName] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [parentCategory, setParentCategory] = useState("");
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  const fetchCategories = () => {
+    axios.get("/api/categories/").then((result) => {
+      setCategories(result.data);
+    });
+  };
   const saveCategory = async (ev) => {
     ev.preventDefault();
-    const response = await axios.post("/api/categories", { name });
-    console.log("response:", response);
+    await axios.post("/api/categories", { name, parentCategory });
     setName("");
+    fetchCategories();
   };
   return (
     <Layout>
@@ -24,10 +34,42 @@ export default function Categories() {
           }}
           value={name}
         />
+        <select
+          className="mb-0"
+          onChange={(ev) => setParentCategory(ev.target.value)}
+          value={parentCategory}
+        >
+          <option value="">No Parent Category</option>
+          {categories.length > 0 &&
+            categories.map((category) => {
+              return (
+                <option key={category._id} value={category._id}>
+                  {category.name}
+                </option>
+              );
+            })}
+        </select>
         <button type="submit" className="btn-primary py-1">
           Save
         </button>
       </form>
+      <table className="basic mt-4">
+        <thead>
+          <tr>
+            <td>Category name</td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => {
+              return (
+                <tr key={category._id}>
+                  <td>{category.name}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </Layout>
   );
 }
