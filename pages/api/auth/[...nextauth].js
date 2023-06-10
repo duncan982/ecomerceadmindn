325ce151b87f0b671357402fcacd6a2a7c9ce043
duncan982 <ndegwaduncan@icloud.com>
2023-06-10 09:37:48 +0200

    restricts login to only recognised emails

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
+const adminEmails = [process.env.EMAIL];
 export default NextAuth({
   providers: [
     GoogleProvider({
@@ -10,4 +11,15 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({ session, token, user }) => {
+      // console.log("session:", session, "token:", token, "user:", user);
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        // console.log("access denied");
+        return false;
+      }
+    },
+  },
 });
