6adf283e55938df8fff34faa73a12e72a328d1fd
duncan982 <ndegwaduncan@icloud.com>
2023-06-14 07:12:15 +0200

    modified to conditionally render navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,11 @@
 // "use client";
 import Nav from "@/components/Nav";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useState } from "react";
+import Logo from "./Logo";
 export default function Layout({ children }) {
   const { data: session } = useSession();
+  const [showNav, setShowNav] = useState(false);
 
   if (!session) {
     return (
@@ -22,10 +25,37 @@ export default function Layout({ children }) {
   }
 
   return (
-    <div className="bg-grey-200 min-h-screen flex">
-      <Nav />
-      <div className="bg-white flex-grow mt-2 mb-2 mr-2 rounded-lg p-4">
-        {children}
+    <div className="bg-grey-200 min-h-screen">
+      <div className="md:hidden flex items-center p-4">
+        <button
+          onClick={() => {
+            setShowNav(true);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <div className="flex grow justify-center mr-6">
+          <Logo />
+        </div>
+      </div>
+      <div className="flex">
+        <Nav show={showNav} />
+        <div className="bg-white flex-grow mt-2 mb-2 mr-2 rounded-lg p-4">
+          {children}
+        </div>
       </div>
     </div>
   );
